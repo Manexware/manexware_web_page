03a526dd79fa86834f63aafd4cf34e7afbe3ea50
luienper <enrique.perez@manexware.com>
2016-07-26 11:59:25 -0500

    [FIX] .idea

---
+++ theme_manexware/__openerp__.py
@@ -0,0 +1,11 @@
+{
+  'name':'Manexware theme',
+  'description': 'A description for your theme.',
+  'version':'1.0',
+  'author':'Manexware',
+
+  'data': [
+  ],
+  'category': 'Theme/Creative',
+  'depends': ['website'],
+}
